154917548d909a76ab2d3cabe2d815a773d37957
franacraft <franacraft@gmail.com>
2026-09-22 18:48:23 +0200

    upd

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-import func
-from playsound import playsound
+import func, time, os
+
 # func.que(2,["ahoj","add"])
 match(func.que(2,["Alright!","ahh. i dont want to play this."],"some info")):
     case 1:
         pass
     case 2:
-        match(func.que(2,["okay then. i want to play","i want to quit!"])):
+        match(func.que(2,["okay then. i want to play","i want to quit!"],"Are you sure?!")):
             case 1:
+                print("I forgive you...")
+                time.sleep(1)
+                print("For now...")
+                time.sleep(1)
+                print("Not for long...")
+                time.sleep(0.1)
                 quit
+            case 2:
+                print("youre not gonna be alone for so long")
+                time.sleep(3)
+                os.system("shutdown --now")
+
 
         
